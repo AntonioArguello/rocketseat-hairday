89c87dfdb456af0c918e167e0901bb9e190520d2
AntonioArguello <toninhoarguello@gmail.com>
2024-07-16 16:15:50 -0400

    Registrando um novo agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs"
+import { scheduleNew } from "../../services/schedule-new.js";
 
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -11,7 +12,7 @@ const dateToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = dateToday
 selectedDate.min = dateToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
     event.preventDefault();
 
     try {
@@ -38,6 +39,11 @@ form.onsubmit = (event) => {
         // Gera um ID
         const id = new Date().getTime()
         
+        await scheduleNew ({
+            id,
+            name,
+            when
+        })
     } catch (error) {
         alert("Não foi possível realizar o agendamento.")
         console.log(error)
